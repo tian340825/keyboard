59bd81e2a72365c18345de0284365ab5546308bc
Yuan <70358680+tian340825@users.noreply.github.com>
2023-10-24 10:05:36 +0800

    Update keyboard.h

--- User/inc/keyboard.h
+++ User/inc/keyboard.h
@@ -10,12 +10,21 @@
  */
 #ifndef __KEYBOARD_H__
 #define __KEYBOARD_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include <string.h>
+#include "stdio.h"
+#include "usb_device.h"
+#include "usbd_customhid.h"
 #include "main.h"
 #include "usart.h"
 #include "usb_device.h"
 #include "gpio.h"
-#include <string.h>
-#include "stdio.h"
+
+#define CHIP_74HC65_TOTAL           11
+#define CHIP_74HC65_PIN_TOTAL       8
 
 typedef enum
 {
@@ -25,18 +34,17 @@ typedef enum
 
 typedef enum SpecialKeyCode_t
 {
-    LEFT_CTRL = 1,LEFT_SHIFT ,LEFT_ALT,LEFT_GUI,RIGHT_CTRL,RIGHT_SHIFT,RIGHT_ALT,RIGHT_GUI
+    LEFT_CTRL = -8,LEFT_SHIFT ,LEFT_ALT,LEFT_GUI,RIGHT_CTRL,RIGHT_SHIFT,RIGHT_ALT,RIGHT_GUI
 }SpecialKeyCode_t;
 
+/**
+ * @brief  HID report data
+ */
 typedef enum NormalKeyCode_t
 {
-    /*------------------------- HID report data -------------------------*/
-    /**
-     * @brief  
-     */
     RESERVED = 0,ERROR_ROLL_OVER,POST_FAIL,ERROR_UNDEFINED,
-    A_CODE,B_CODE,C_CODE,D_CODE,E_CODE,F_CODE,G_CODE,H_CODE,I_CODE,J_CODE,K_CODE,L_CODE,M_CODE,
-    N_CODE,O_CODE,P_CODE,Q_CODE,R_CODE,S_CODE,T_CODE,U_CODE,V_CODE,W_CODE,X_CODE,Y_CODE,Z_CODE,
+    CODE_A,CODE_B,CODE_C,CODE_D,CODE_E,CODE_F,CODE_G,CODE_H,CODE_I,CODE_J,CODE_K,CODE_L,CODE_M,
+    CODE_N,CODE_O,CODE_P,CODE_Q,CODE_R,CODE_S,CODE_T,CODE_U,CODE_V,CODE_W,CODE_X,CODE_Y,CODE_Z,
     NUM_1/*1!*/,NUM_2/*2@*/,NUM_3/*3#*/,NUM_4/*4$*/,NUM_5/*5%*/,
     NUM_6/*6^*/,NUM_7/*7&*/,NUM_8/*8**/,NUM_9/*9(*/,NUM_0/*0)*/,
     ENTER,ESC,BACKSPACE,TAB,SPACE,
@@ -53,19 +61,38 @@ typedef enum NormalKeyCode_t
     F13,F14,F15,F16,F17,F18,F19,F20,F21,F22,F23,F24, EXECUTE,
     HELP,MENU,SELECT,STOP,AGAIN,UNDO,CUT,COPY,PASTE,FIND,MUTE,VOLUME_UP,VOLUME_DOWN,
     FN = 1000
-    /*------------------------- HID report data -------------------------*/
 }NormalKeyCode_t;
 
 
-
-    
+/**
+ * @brief  
+ */
 typedef struct keyReportDada
 {
-    uint8_t  specialKey;
-    uint8_t  reserve;
-    uint8_t  NormalKey[17];
+    uint8_t  specialKeyCode;
+    uint8_t  reserved;
+    uint8_t  normalKeyCode[17];
 }keyReportDada;
-KeyStatus Key_Scan(GPIO_TypeDef* GPIOx,uint16_t GPIO_Pin);
 
-void Key_Test(KeyStatus status);
-#endif 
+typedef struct keyBoard_t
+{
+    keyReportDada reportData;
+    int reportDataLen;
+    uint8_t keyPad[CHIP_74HC65_TOTAL];
+    uint8_t reportFlg;
+    int  keyRawData[CHIP_74HC65_TOTAL][CHIP_74HC65_PIN_TOTAL];
+
+    int8_t (*updataReportData) (struct keyBoard_t *,uint8_t ,uint8_t* );
+    int (*scan) (struct keyBoard_t *,GPIO_TypeDef* ,uint16_t );
+	void (*test) (struct keyBoard_t *);
+    void (*printReportData) (struct keyBoard_t *);
+    void (*printKeyRawData) (struct keyBoard_t *);
+}keyBoard_t;
+
+void keyInit(keyBoard_t *k);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //__KEYBOARD_H__
